0e7831490b1305fa00dd46ba34a756111d94782f
Joelma Rodrigues <113507468+joelmarodrigues@users.noreply.github.com>
2023-04-03 15:27:15 +0100

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,52 +1,28 @@
 import { Sequelize } from "sequelize";
-
-/* Server */
+ 
 const server = {
-  database: "DevOps_Calendar",
+  database: "devops_calendar",
   username: "root",
-  password: "#QN-*E$dT+@^i,YH",
+  password: "1234mudar",
   host: "35.193.37.252",
   dialect: "mysql",
   define: {
     timestamps: false,
   },
 };
-
-
-/* Localhost */
-const localhost = {
-  database: "DevOps_Calendar", 
-  username: "root", // Change this to your username
-  password: "gds437", // Change this to your password
-  host: "localhost",
-  dialect: "mysql",
-  define: {
-    timestamps: false,
-  },
-};
-
-const db1 = new Sequelize(server);
-const db2 = new Sequelize(localhost);
-
+ 
+const db = new Sequelize(server);
+ 
 const connectDb = async () => {
   try {
-    await db1.authenticate();
-    console.log("Connected to server successfully");
-    await db1.sync({ force: false });
-    return db1;
+    await db.authenticate();
+    console.log("Connected to database successfully");
+    await db.sync({ force: false });
+    return db;
   } catch (error) {
-    console.error("Unable to connect to server:", error.message);
-
-    try {
-      await db2.authenticate();
-      console.log("Connected to localhost successfully");
-      await db2.sync({ force: false });
-      return db2;
-    } catch (error) {
-      console.error("Unable to connect to localhost:", error.message);
-      return null;
-    }
+    console.error("Unable to connect to database:", error.message);
+    return null;
   }
 };
-
+ 
 export default connectDb;
